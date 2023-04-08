97604e7c70e8a5a5eec07269e8813a66a6ee6772
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2023-04-08 10:11:05 -0300

    example code text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import CodeText from "./pages/modules/components/CodeText/CodeText";
+import ExampleCodeText from "./pages/modules/components/CodeText/ExampleCodeText";
 
 export default function Home() {
   return (
     <main>
-      <div className="ml-1 rounded-2xl shadow-lg w-4/12 bg-gray-800">
-        <CodeText className="font-bold text-3xl border-b-2 border-cyan-400">
-          WHATS AMT?
-        </CodeText>
-        <CodeText preserveCursor={false}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui est hic
-          doloribus ipsam laborum. Quisquam nam neque sunt dolore atque maxime
-          vero adipisci ea amet, reprehenderit in modi error magnam.
-        </CodeText>
-      </div>
+      <ExampleCodeText />
     </main>
   );
 }
