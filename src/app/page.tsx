97a6715c31b851052141b10d19d4f56ff1733b0d
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2023-04-07 21:45:03 -0300

    styles on the father

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import CodeText from "./pages/modules/components/CodeText/CodeText";
 export default function Home() {
   return (
     <main>
-      <CodeText className="font-bold text-3xl" preserveCursor={false}>
+      <CodeText
+        className="font-bold text-3xl border-b-2 border-cyan-400"
+        preserveCursor={false}
+      >
         WHATS AMT?
       </CodeText>
     </main>
--- src/app/pages/modules/components/CodeText/CodeText.tsx
+++ src/app/pages/modules/components/CodeText/CodeText.tsx
@@ -5,7 +5,7 @@ import { useCallback, useLayoutEffect, useMemo, useState } from "react";
 const CodeText = ({ children, className, preserveCursor }: IProps) => {
   const [text, setText] = useState<Array<string>>([]);
   const textModifier = useCallback((l: string, i: number) => {
-    setTimeout(() => setText((text) => [...text, l]), 100 * i);
+    setTimeout(() => setText((text) => [...text, l]), 75 * i);
   }, []);
   const splitedText = useMemo(() => children.split(""), [children]);
   const joinedText = useMemo(() => text.join(""), [text]);
@@ -15,10 +15,8 @@ const CodeText = ({ children, className, preserveCursor }: IProps) => {
   }, [splitedText, textModifier]);
 
   return (
-    <div className="p-2">
-      <p
-        className={`bg-gray-800 text-white text-bold p-1 px-4 shadow-xl rounded-lg font-mono w-fit ${className}`}
-      >
+    <div className="ml-2 p-2 bg-gray-800 text-white text-bold px-4 shadow-xl rounded-lg w-fit">
+      <p className={`font-mono  ${className}`}>
         {joinedText}
         {(joinedText === children && !preserveCursor) || (
           <span className="after:content-[' '] after:border-r-4 after:animate-pulse after:text-gray-400 after:ml-1" />
