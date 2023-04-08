991d7378c0052c3d9fbba8a22b06afb456b5ec94
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2023-04-07 21:41:14 -0300

    hiding the cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import CodeText from "./pages/modules/components/CodeText/CodeText";
 export default function Home() {
   return (
     <main>
-      <CodeText className="font-bold text-3xl" preserveCursor={true}>
+      <CodeText className="font-bold text-3xl" preserveCursor={false}>
         WHATS AMT?
       </CodeText>
     </main>
