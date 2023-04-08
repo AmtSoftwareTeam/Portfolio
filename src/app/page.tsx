d9d4810ffdbffecb145978eb570419fa035aac24
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2023-04-08 10:05:17 -0300

    example mutiple code text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import CodeText from "./pages/modules/components/CodeText/CodeText";
 export default function Home() {
   return (
     <main>
-      <CodeText
-        className="font-bold text-3xl border-b-2 border-cyan-400"
-        preserveCursor={false}
-      >
-        WHATS AMT?
-      </CodeText>
+      <div className="ml-1 rounded-2xl shadow-lg w-4/12 bg-gray-800">
+        <CodeText className="font-bold text-3xl border-b-2 border-cyan-400">
+          WHATS AMT?
+        </CodeText>
+        <CodeText preserveCursor={false}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui est hic
+          doloribus ipsam laborum. Quisquam nam neque sunt dolore atque maxime
+          vero adipisci ea amet, reprehenderit in modi error magnam.
+        </CodeText>
+      </div>
     </main>
   );
 }
